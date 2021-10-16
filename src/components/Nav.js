5f98aa75f67b55b47dc8225a0fa7456ddd1d3d84
solenedel <solene98@student.ubc.ca>
2021-10-16 15:31:09 -0700

    create Nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav>
+      <div className="logo">café finder</div>
+      <ul>
+        <li>Login</li>
+        <li>Favourites</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
