b88119acef0ab699db53664bdbe19e095e72dc0e
solenedel <solene98@student.ubc.ca>
2021-10-17 12:46:46 -0700

    create HomePage component

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <main id="home-page-container">
+      <div>Find cafés nearby.</div>
+      <img src="../../public/images/coffee-1.png" alt="coffee icon" />
+    </main>
+  );
+};
+
+export default HomePage;
