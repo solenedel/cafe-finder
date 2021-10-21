6dc50c99acc25a11c26167173d5987cc54c05486
solenedel <solene98@student.ubc.ca>
2021-10-21 15:06:37 -0700

    change MapConteiner to Map component

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { GoogleMap } from 'react-google-maps';
 
 const Map = () => {
-  return <GoogleMap />;
+  return <GoogleMap defaultZoom={10} defaultCenter={{ lat: 12, lng: 12 }} />;
 };
 
 export default Map;
