4e59e130a91b73037a2a2ad1f7e9670d4c6c0842
solenedel <solene98@student.ubc.ca>
2021-10-21 20:26:44 -0700

    map is workinggit add .

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
 
 export const Map = () => {
-  return <GoogleMap defaultZoom={10} defaultCenter={{ lat: 12, lng: 12 }} />;
+  return <GoogleMap defaultZoom={10} defaultCenter={{ lat: 49.24966, lng: -123.11934 }} />;
 };
 
 // WrappedMap is the component we want to insert into the App
