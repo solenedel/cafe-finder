b005831b7bb7d4d7741c91dd06651b32359f84c1
solenedel <solene98@student.ubc.ca>
2021-10-22 13:59:45 -0700

    load map with react-google-maps/api, remove previous map code

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,23 +1,8 @@
 import React from 'react';
-import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps/api';
-import { WrappedMap } from './Map';
+import Map from './Map';
 
 // eslint-disable-next-line
 export const HomePage = ({ className }) => {
-  const libraries = ['places'];
-
-  // load Google Map scripts
-
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_API_KEY,
-    libraries,
-  });
-
-  if (loadError) return 'Error loading maps';
-  if (!isLoaded) return 'Loading maps...';
-
-  // finished loading Google Map scripts
-
   return (
     <main className={className} id="home-page-container">
       <div>Find cafés near you.</div>
@@ -31,12 +16,7 @@ export const HomePage = ({ className }) => {
         </form>
       </section>
       <div id="map-div" style={{ width: '80vw', height: '100vh' }}>
-        <WrappedMap
-          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_API_KEY}`}
-          loadingElement={<div style={{ height: '100%' }} />}
-          containerElement={<div style={{ height: '100%' }} />}
-          mapElement={<div style={{ height: '100%' }} />}
-        />
+        <Map />
       </div>
     </main>
   );
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,32 @@
 import React from 'react';
-import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
+// eslint-disable-next-line
+import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 
-export const Map = () => {
-  return <GoogleMap defaultZoom={11} defaultCenter={{ lat: 49.24966, lng: -123.11934 }} />;
+// eslint-disable-next-line
+const Map = () => {
+  const libraries = ['places'];
+  const mapContainerStyle = {
+    width: '80vw',
+    height: '100vh',
+  };
+  const center = {
+    lat: 49.24966,
+    lng: -123.11934,
+  };
+
+  // load Google Map scripts
+
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_API_KEY,
+    libraries,
+  });
+
+  if (loadError) return 'Error loading maps';
+  if (!isLoaded) return 'Loading maps...';
+
+  // finished loading Google Map scripts
+
+  return <GoogleMap mapContainerStyle={mapContainerStyle} zoom={8} center={center} />;
 };
 
-// WrappedMap is inserted into HomePage component
-export const WrappedMap = withScriptjs(withGoogleMap(Map));
+export default Map;
