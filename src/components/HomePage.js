481672491b13be735213c3e33aee589fd0e76774
solenedel <solene98@student.ubc.ca>
2021-10-21 18:24:11 -0700

    add google maps api key

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -9,7 +9,7 @@ export const HomePage = ({ className }) => {
       <img src="./images/coffee-1.png" alt="coffee icon" />
       <div style={{ width: '100vw', height: '100vh' }}>
         <WrappedMap
-          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_API_KEY}`}
           loadingElement={<div style={{ height: '100%' }} />}
           containerElement={<div style={{ height: '100%' }} />}
           mapElement={<div style={{ height: '100%' }} />}
