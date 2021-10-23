65b9b6b97bf30eb8c197e8079424a924eb46130b
solenedel <solene98@student.ubc.ca>
2021-10-23 14:09:24 -0700

    create schema for users, cafes, favourites

---
+++ server/db/schema.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS cafes CASCADE;
+DROP TABLE IF EXISTS favorites CASCADE;
+
+-- logged in users have more features available
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  username VARCHAR(255) NOT NULL, 
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(60) NOT NULL,
+);
+
+CREATE TABLE cafes (
+  id SERIAL PRIMARY KEY NOT NULL,
+  cafe_name VARCHAR(255) NOT NULL,
+  has_wifi BOOLEAN,
+  open_24_hours BOOLEAN,
+  organic_tea_coffee BOOLEAN,
+  noise_level INTEGER,
+  seating_space INTEGER,
+);
+-- NOTE: noise_level and seating_space are integers from 1 to 5 (stars system)
+
+-- Join table
+CREATE TABLE favorites (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  cafe_id INTEGER REFERENCES cafes(id) ON DELETE CASCADE,
+);
