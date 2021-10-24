9610cb21158399b0fa40735b4ed05631de1ddba2
solenedel <solene98@student.ubc.ca>
2021-10-24 11:25:15 -0700

    create routes directory, start search route

---
+++ server/routes/search.js
@@ -0,0 +1,7 @@
+// -------------------------- search route ----------------------------- //
+
+// This route uses the Google Places API to search for cafes in a location specified by the user
+const express = require('express');
+require('dotenv').config();
+
+const router = express.Router();
