30a78211b8bde376b9080d63e2b78ddf9f5fc9b8
solenedel <solene98@student.ubc.ca>
2021-10-24 16:28:00 -0700

    debugging api key error

--- server/routes/search.js
+++ server/routes/search.js
@@ -4,4 +4,23 @@
 const express = require('express');
 require('dotenv').config();
 
+// eslint-disable-next-line
 const router = express.Router();
+const axios = require('axios');
+
+// helper: make request to Google Places API
+const cafeSearchHelper = (location) => {
+  axios
+    .get(
+      `https://maps.googleapis.com/maps/api/place/textsearch/json
+    ?query=cafes%20in%20${location}
+    &key=${process.env.REACT_APP_API_KEY}`
+    )
+    .then((res) => res.data)
+    .catch((error) => error.message);
+};
+
+// TO DO: debug , api key undefined
+console.log(process.env.REACT_APP_API_KEY);
+
+console.log('cafes in vancouver: ', cafeSearchHelper('Vancouver'));
