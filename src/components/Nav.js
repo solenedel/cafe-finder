f59bbdc94211a6fda3642bc20e403e0ae8f4c58a
solenedel <solene98@student.ubc.ca>
2021-10-27 19:04:09 -0700

    create useContext hook

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { useAppContext } from '../context';
 
 // eslint-disable-next-line
 const Nav = ({ className }) => {
+  const { userContext } = useAppContext();
+  const [user, setUser] = userContext;
+
+  const LoginButton = () => (
+    <Link to="/login">
+      <li>Login</li>
+    </Link>
+  );
+
+  const handleLogout = () => {
+    axios.post('/logout').then((res) => {
+      setUser((prev) => ({
+        ...prev,
+        auth: res.data.auth,
+      }));
+    });
+  };
+
+  const LogoutButton = () => (
+    <Link to="/logout">
+      <li>Logout</li>
+    </Link>
+  );
+
   return (
     <nav className={className}>
       <div id="nav-flexbox">
@@ -11,12 +37,10 @@ const Nav = ({ className }) => {
         </Link>
 
         <ul>
-          <Link to="/login">
-            <li>Login</li>
-          </Link>
           <Link to="/favourites">
             <li>Favourites</li>
           </Link>
+          {user.auth ? LogoutButton() : LoginButton()}
         </ul>
       </div>
     </nav>
