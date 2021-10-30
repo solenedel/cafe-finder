9271037824bd057d4db6055435086d5c77211988
solenedel <solene98@student.ubc.ca>
2021-10-30 11:38:17 -0700

    create FavCafe component to hold a single fav

---
+++ src/components/FavCafe.js
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+const FavCafe = (props) => {
+  const { fav, removeFavCafe } = props;
+
+  return (
+    <div className="favCafe">
+      <h4>{fav.cafe_name}</h4>
+      <ul>
+        <li>Wifi: {fav.has_wifi ? 'yes' : 'no'}</li>
+        <li>Open 24 hours: {fav.is_open_24_hours ? 'yes' : 'no'}</li>
+        <li>Organic coffee/tea: {fav.has_organic_tea_coffee ? 'yes' : 'no'}</li>
+        <li>noise level: {fav.noise_level}</li>
+      </ul>
+      <button type="button" onClick={() => removeFavCafe(fav.id)}>
+        Remove
+      </button>
+    </div>
+  );
+};
+
+export default FavCafe;
