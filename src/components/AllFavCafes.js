c55bc243e809eb6a69fbc809c58cf21e186c39a2
solenedel <solene98@student.ubc.ca>
2021-10-30 11:46:04 -0700

    updating AllFavCafes component

--- src/components/AllFavCafes.js
+++ src/components/AllFavCafes.js
@@ -1,33 +1,29 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import FavCafe from './FavCafe';
+import { useAppContext } from '../context';
 
 // eslint-disable-next-line
 export const AllFavCafes = (props) => {
   // eslint-disable-next-line
   const { className, favCafes, removeFavCafe } = props;
+  const { userContext } = useAppContext();
+  // eslint-disable-next-line
+  const [user, setUser] = userContext;
 
   console.log('favCafes: ', favCafes);
 
   // show a user's favourite cafes
   const showFavCafes = () => {
-    // eslint-disable-next-line
     if (!favCafes.length) {
       return <p>You have not added any cafés to your favourites.</p>;
     }
+
     // eslint-disable-next-line
     return favCafes.map((fav) => {
-      return (
-        <div className="favCafe">
-          <h4>{fav.cafe_name}</h4>
-          <ul>
-            <li>Wifi: {fav.has_wifi ? 'yes' : 'no'}</li>
-            <li>Open 24 hours: {fav.is_open_24_hours ? 'yes' : 'no'}</li>
-            <li>Organic coffee/tea: {fav.has_organic_tea_coffee ? 'yes' : 'no'}</li>
-            <li>noise level: {fav.noise_level}</li>
-          </ul>
-          <button type="button">Remove</button>
-        </div>
-      );
+      return <FavCafe key={fav.id} fav={fav} removeFavCafe={removeFavCafe} />;
     });
   };
+
   return <div id="favCafeList">{user.auth ? showFavCafes() : ''}</div>;
 };
